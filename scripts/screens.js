d36be87bfbf33582fe95f0c786974af17663c052
DevelopedBy-Siva <developedby.siva@gmail.com>
2021-10-15 22:55:44 +0530

    Optimise browser performance on window resize

--- scripts/screens.js
+++ scripts/screens.js
@@ -2,12 +2,31 @@ let START_GAME = false;
 
 const gameContainer = document.getElementsByClassName("game-container");
 
+// Optimise browser performance by triggering the event only after few "ms"
+const handleWindowResize = () => {
+    let execute = false;
+    return () => {
+        
+        if(execute)
+            return;
+
+        execute = true;
+        setTimeout(() => {
+
+            if(START_GAME)
+                gameContainer[0].scrollTop = window.innerHeight;
+            else
+                gameContainer[0].scrollTop = 0
+
+            execute = false;
+        }, 300);
+    }
+}
+
+const resizeScreen = handleWindowResize();
+
 // Make sure resizing the window doesn't create a screen conflict
-window.addEventListener("resize", () => {
-    if(START_GAME)
-        return gameContainer[0].scrollTop = window.innerHeight
-    gameContainer[0].scrollTop = 0
-});
+window.addEventListener("resize", resizeScreen);
 
 // Move between the screen based on button "CLICK"
 export const moveBetweenScreens = () => {
