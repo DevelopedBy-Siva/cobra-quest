36f18c06ca34ed440991cd2152b04b20d84d622b
DevelopedBy-Siva <developedby.siva@gmail.com>
2021-10-24 19:20:33 +0530

    Improve web page performance

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,30 @@ import { addPlayer, handleInputChange, handlePlayers, handlePlayersClose, loadPl
 import { AllPlayers } from "./player_s_Object_s.js";
 import { moveBetweenScreens } from "./screens.js";
 
-// Handle Loading screen
-window.addEventListener("load" ,() => {
-    const container = document.getElementById("loading-screen");
-    const spinner = document.getElementById("loading-screen-spinner");
-    spinner.classList.add("loading-screen-animation-close");
-    container.classList.add("loading-screen-animation");
-    setTimeout(() => {
-        container.remove();
-    },1000)
-});
+const container = document.getElementById("loading-screen");
+const spinner = document.getElementById("loading-screen-spinner");
+const playerWrapper = document.getElementsByClassName("player-wrapper")[0];
+const gameTitle = document.querySelector(".game-title h4");
+const gameImage = document.querySelector(".game-title img");
 
 // Game Start/ Exit Buttons
 const startButton = document.getElementById("start");
 const exitButton = document.getElementById("exit");
+
+// Handle Loading screen
+window.addEventListener("load" ,() => {
+    
+    spinner.classList.add("loading-screen-animation-close");
+    container.classList.add("loading-screen-animation");
+    playerWrapper.classList.add("display-player-wrapper");
+    gameTitle.classList.add("game-title-anim");
+    gameImage.classList.add("game-logo-anim");
+    startButton.classList.add("game-start-btn-anim");
+
+    setTimeout(() => {
+        container.remove();
+    },1000)
+});
 
 // What to do when user Click "START"/ "EXIT"
 startButton.addEventListener("click", moveBetweenScreens);
